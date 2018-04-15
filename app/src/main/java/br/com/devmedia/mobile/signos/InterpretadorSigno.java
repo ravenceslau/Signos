cba057d03cfc9b75d752783834f8115571edb994
Robson Venceslau <ravences@gmail.com>
2018-04-15 01:07:16 -0300

    Correção do erro ao selecionar a data referente ao signo de áries e virgens.

--- app/src/main/java/br/com/devmedia/mobile/signos/InterpretadorSigno.java
+++ app/src/main/java/br/com/devmedia/mobile/signos/InterpretadorSigno.java
@@ -11,7 +11,7 @@ public class InterpretadorSigno {
     private ArrayList<Signo> signos = new ArrayList<Signo>(){{
        add(new Signo(20,1,18,2,"Aquário","@drawable/aquario"));
        add(new Signo(19,2,20,3,"Peixes","@drawable/peixes"));
-       add(new Signo(21,3,19,4,"Áries","@drawalable/aries"));
+       add(new Signo(21,3,19,4,"Áries","@drawable/aries"));
        add(new Signo(20,4,20,5,"Touro","@drawable/touro"));
        add(new Signo(21,5,20,6,"Gêmeos","@drawable/gemeos"));
        add(new Signo(21,6,22,7,"Câncer","@drawable/cancer"));
